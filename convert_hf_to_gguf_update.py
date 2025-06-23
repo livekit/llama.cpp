becf2b13c1a65c9ac4bfc3edb79aa56003165633
David Zhao <dz@livekit.io>
2025-06-22 20:00:37 -0700

    support livekit-turn model

--- convert_hf_to_gguf_update.py
+++ convert_hf_to_gguf_update.py
@@ -128,6 +128,7 @@ models = [
     {"name": "llama4",           "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/meta-llama/Llama-4-Scout-17B-16E-Instruct", },
     {"name": "pixtral",          "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/mistral-community/pixtral-12b", },
     {"name": "seed-coder",       "tokt": TOKENIZER_TYPE.BPE, "repo": "https://huggingface.co/ByteDance-Seed/Seed-Coder-8B-Base", },
+    {"name": "livekit-turn",     "tokt": TOKENIZER_TYPE.BPE, "repo": os.getenv("TURN_DETECTOR_MODEL_URL")},
 ]
 
 # some models are known to be broken upstream, so we will skip them as exceptions
